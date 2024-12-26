8350c1e61caa2bc08fb76f0b47f7d823c11bf2b1
Máximo Fernández Riera <maxfernandez@gmail.com>
2024-12-26 20:53:53 +0100

    Next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Esta línea es la más importante para corregir el error
+  basePath: '/dragonball-app', // Reemplaza 'dragonball-app' con el nombre de tu repositorio
+  assetPrefix: '/dragonball-app/', // Reemplaza 'dragonball-app' con el nombre de tu repositorio
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
