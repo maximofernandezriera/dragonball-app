9c9e083e83576340822fb7ae480a3aa1bf3cda35
maximofernandezriera <maxfernandez@gmail.com>
2024-12-27 09:05:34 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Fundamental para el modo estático en GitHub Pages
-  basePath: '/dragonball-app', // Reemplaza 'dragonball-app' con el nombre de TU repositorio.
-  assetPrefix: '/dragonball-app/', // Reemplaza 'dragonball-app' con el nombre de TU repositorio.
+  output: 'export',
+  basePath: '/dragonball-app',
+  assetPrefix: '/dragonball-app/',
   images: {
-    unoptimized: true, // Desactiva la optimización de imágenes de Next.js
+    unoptimized: true,
   },
 };
 
-module.exports = nextConfig;  
+module.exports = nextConfig;
