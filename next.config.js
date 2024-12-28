8503f618af8d681f13403d510a0eda1246586390
maximofernandezriera <maxfernandez@gmail.com>
2024-12-28 14:25:38 +0100

    mATÍ

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'export',
   basePath: '/dragonball-app',
   assetPrefix: '/dragonball-app/',
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    turbo: false, // Deshabilita Turbopack
   },
 };
 
