5603b5f35f8582cf6badf689f323a656124226c3
maximofernandezriera <maxfernandez@gmail.com>
2024-12-28 01:46:11 +0100

    ASAsa

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[{"source":"/dragonball-app//_next/:path+","destination":"/dragonball-app/_next/:path+"}],"fallback":[]},"/_app":["static/chunks/pages/_app.js"],"/_error":["static/chunks/pages/_error.js"],"sortedPages":["/_app","/_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
